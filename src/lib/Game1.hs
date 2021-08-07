8f7dacd6866da0355931dc6f73f5fe9821d2d716
erikbackman <erikbackman@users.noreply.github.com>
2021-08-07 21:26:33 +0200

    style: format using brittany

--- src/lib/Game1.hs
+++ src/lib/Game1.hs
@@ -3,32 +3,57 @@
 
 module Game1 where
 
-import           Control.Concurrent     (threadDelay)
-import           Control.Lens           ((%=), (%~), (+=), (.=), (^.))
-import           Control.Monad.IO.Class (MonadIO)
-import           Control.Monad.Reader   (MonadReader (..),
-                                         ReaderT (ReaderT, runReaderT), asks,
-                                         reader, runReader)
-import           Control.Monad.State    (MonadIO (..), MonadState, StateT,
-                                         evalStateT, gets)
-import           Data.Foldable          (foldl')
-import           Data.Monoid            (Any (Any))
-import           Data.Semigroup         (Sum (Sum))
-import           Data.Text              (Text)
-import           Foreign.C              (CInt)
-import           Paths_game1            ()
-import           SDL                    (Point (P), Renderer, V2 (V2), _x, _y,
-                                         ($=))
+import           Control.Concurrent             ( threadDelay )
+import           Control.Lens                   ( (%=)
+                                                , (%~)
+                                                , (+=)
+                                                , (.=)
+                                                , (^.)
+                                                )
+import           Control.Monad.IO.Class         ( MonadIO )
+import           Control.Monad.Reader           ( MonadReader(..)
+                                                , ReaderT(ReaderT, runReaderT)
+                                                , asks
+                                                , reader
+                                                , runReader
+                                                )
+import           Control.Monad.State            ( MonadIO(..)
+                                                , MonadState
+                                                , StateT
+                                                , evalStateT
+                                                , gets
+                                                )
+import           Data.Foldable                  ( foldl' )
+import           Data.Monoid                    ( Any(Any) )
+import           Data.Semigroup                 ( Sum(Sum) )
+import           Data.Text                      ( Text )
+import           Foreign.C                      ( CInt )
+import           Paths_game1                    ( )
+import           SDL                            ( ($=)
+                                                , Point(P)
+                                                , Renderer
+                                                , V2(V2)
+                                                , _x
+                                                , _y
+                                                )
 
-import           Game1.GameState        (GameState (..), playerPos)
-import           Game1.Input            (Intent (Idle, Move, Quit),
-                                         inputToIntent, pollEventPayloads)
-import           Game1.Player           (nextPlayerPos, renderPlayer,
-                                         startPosition)
-import           Game1.Resources        (Resources (..), withResources)
-import           Game1.Window           (withWindow)
+import           Game1.GameState                ( GameState(..)
+                                                , playerPos
+                                                )
+import           Game1.Input                    ( Intent(Idle, Move, Quit)
+                                                , inputToIntent
+                                                , pollEventPayloads
+                                                )
+import           Game1.Player                   ( nextPlayerPos
+                                                , renderPlayer
+                                                , startPosition
+                                                )
+import           Game1.Resources                ( Resources(..)
+                                                , withResources
+                                                )
+import           Game1.Window                   ( withWindow )
 
-import           Game1.Scene            (drawScene)
+import           Game1.Scene                    ( drawScene )
 
 import qualified SDL
 import qualified SDL.Image
@@ -37,12 +62,13 @@ main :: IO ()
 main = do
   SDL.initialize [SDL.InitVideo]
 
-  withWindow "Game1" windowConfig \w ->
-    withResources w \r -> runGame1 r initState mainLoop
+  withWindow "Game1"
+             windowConfig
+             \w -> withResources w \r -> runGame1 r initState mainLoop
 
-  where
-    initState = GameState { _playerPos = startPosition }
-    windowConfig = SDL.defaultWindow
+ where
+  initState    = GameState { _playerPos = startPosition }
+  windowConfig = SDL.defaultWindow
 
 newtype Game1 a =
   Game1 (ReaderT Resources (StateT GameState IO) a)
@@ -51,7 +77,8 @@ newtype Game1 a =
 runGame1 :: Resources -> GameState -> Game1 a -> IO a
 runGame1 r s (Game1 m) = evalStateT (runReaderT m r) s
 
-mainLoop :: (MonadIO m, MonadReader Resources m, MonadState GameState m) => m ()
+mainLoop
+  :: (MonadIO m, MonadReader Resources m, MonadState GameState m) => m ()
 mainLoop = do
   input <- pollEventPayloads
   case inputToIntent input of
@@ -59,11 +86,11 @@ mainLoop = do
     Idle       -> step
     Move delta -> playerPos %= nextPlayerPos delta >> step
 
-  where
-    step = do
-      r <- asks sdl_renderer
-      SDL.clear r
-      drawScene
-      SDL.present r
-      liftIO $ threadDelay (1000 * 16)
-      mainLoop
+ where
+  step = do
+    r <- asks sdl_renderer
+    SDL.clear r
+    drawScene
+    SDL.present r
+    liftIO $ threadDelay (1000 * 16)
+    mainLoop
