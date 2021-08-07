097a76eeeb4d44c94e584c7084255d0ce36e384f
erikbackman <erikbackman@users.noreply.github.com>
2021-08-08 01:34:10 +0200

    refac: formatting script

--- scripts/formatting-check.sh
+++ scripts/formatting-check.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-files=$(find "./src/app" "./src/lib" -iname '*.hs')
-for file in $files
-do
-    ormolu $file --mode=check
-done
+find "./src/app" "./src/lib" -iname '*.hs' -exec ormolu {} --mode=check \;
