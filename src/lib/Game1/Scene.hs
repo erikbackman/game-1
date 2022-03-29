49b5bdaca918ffdc337a53cbcab34b5c95597179
erikbackman <erikbackman@users.noreply.github.com>
2022-03-29 16:42:56 +0200

    cleanup

--- src/lib/Game1/Scene.hs
+++ src/lib/Game1/Scene.hs
@@ -1,26 +1,17 @@
 module Game1.Scene where
 
 import Control.Lens (use)
-import Control.Monad.IO.Class (MonadIO)
-import Control.Monad.Reader.Class
-  ( MonadReader,
-  )
-import Control.Monad.State.Class
-  ( MonadState,
-  )
 import Game1.GameState (GameState (..), gs_map, gs_player, drawMap)
 import Game1.Player (renderPlayer)
 import Game1.Resources (Resources (..))
 import Game1.Render (renderTexture)
-import Foreign.C
 import SDL.Vect
 import Control.Monad.RWS
 
 renderHeart :: (MonadIO m, MonadReader Resources m) => m ()
 renderHeart = do
   (tex, _) <- asks tex_heart
-  let target = fmap (CInt . (32 *) . fromIntegral) (V2 10 0) 
-  renderTexture tex target (V2 False False)
+  renderTexture tex (V2 10 0) (V2 False False)
   
 drawScene ::
   (MonadIO m, MonadReader Resources m, MonadState GameState m) => m ()
